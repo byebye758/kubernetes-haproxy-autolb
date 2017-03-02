9698647c1c7e4b2cd6edaa57ed4a4108e71f2826
元鹏 <532549992@qq.com>
2017-03-02 15:15:54 +0800

    优化route

--- route.go
+++ route.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"kubernetes-haproxy-autolb/backends/con"
 	"kubernetes-haproxy-autolb/backends/etcd3client"
-	"kubernetes-haproxy-autolb/backends/node"
 	"kubernetes-haproxy-autolb/backends/watch"
-	//"time"
 )
 
 var (
@@ -15,19 +12,13 @@ var (
 )
 
 func main() {
-	// ch := make(chan string)
-	// nodeip := con.HostIP()
-	// dockerip, _ := con.Getdockerip()
-	// a := etcd3client.Autotable{endpoints, "/autohaproxy/autotable/"}
+	ch := make(chan string)
 
 	f := etcd3client.Node{endpoints, "/autohaproxy/node/nodeip/"}
 
 	go watch.Nodeiproutewatch("/autohaproxy/node/nodeip/", endpoints, f, ch)
-	// go watch.Nodenoderoutewatch("/autohaproxy/autotable/", endpoints, a, ch)
-	// go g.NodePutLease()
-	// node.Noderoute(a, endpoints)
-	// // go node.Iproute(f, endpoints)
-	// // go node.Noderoute(a, endpoints)
+
+	node.Iproute(f, endpoints)
 	for {
 
 		fmt.Println(<-ch)
